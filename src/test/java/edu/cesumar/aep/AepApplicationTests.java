97fa4a64404183669d41df2dd9717b778bb8737d
Hugo E. Contreras <ehugo209@gmail.com>
2026-06-11 10:23:18 -0300

    initial commit

---
+++ src/test/java/edu/cesumar/aep/AepApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cesumar.aep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
